23926e18de7e85b5580b7a6d5176f2b9359059b9
anylots <wb-xyk290970@antfin.com>
2020-06-26 18:13:23 +0800

    init project

---
+++ src/main/java/com/frame/detection/DetectionApplication.java
@@ -0,0 +1,13 @@
+package com.frame.detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frame/detection/DetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frame.detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
